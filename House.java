80b27901980efd50dfc073c5b9bbbbe3f1ecb1b6
Dani J <djusto@smith.edu>
2023-10-22 14:20:16 -0400

    First Commit

--- House.java
+++ House.java
@@ -1,8 +1,40 @@
 /* This is a stub for the House class */
-public class House {
+import java.util.ArrayList;
 
-  public House() {
+public class House extends Building {
+  private ArrayList<String> residents; // The <String> tells Java what kind of data we plan to store IN the ArrayList
+  private boolean hasDiningRoom;  
+
+  public House(String name, String address, int nFloors, boolean hasDiningRoom){
+    super(name, address, nFloors);
+    this.hasDiningRoom = hasDiningRoom;
+    this.residents = new ArrayList<>();
     System.out.println("You have built a house: 🏠");
+  }
+
+  public void moveIn(String name){
+    if(this.residents.contains(name) == false){
+      this.residents.add(name);
+    } else {
+      throw new RuntimeException("Resident already lives here!");
+    }
+  }
+
+  public String moveOut(String name){ 
+    if(this.residents.contains(name)){
+      this.residents.remove(name);
+      return name;
+    } else {
+      throw new RuntimeException("Resident not found!");
+    }
+  }
+
+  public boolean isResident(String person){
+    if(this.residents.contains(name)){
+      return true;
+    } else {
+      return false;
+    } 
   }
 
   public static void main(String[] args) {
--- Library.java
+++ Library.java
@@ -1,10 +1,58 @@
 /* This is a stub for the Library class */
-public class Library {
+import java.util.Hashtable;
 
-    public Library() {
+import javax.management.RuntimeErrorException;
+
+public class Library extends Building  {
+  private Hashtable<String, Boolean> collection;
+
+    public Library(String name, String address, int nFloors) {
+      super(name, address, nFloors);
+      this.collection = new Hashtable<>();
       System.out.println("You have built a library: 📖");
     }
   
+  public void addTitle(String title){
+    if(this.collection.contains(title)){
+      throw new RuntimeException("This book already exists in the system!");
+    } else {
+      this.collection.put(title, true);
+    }
+  }
+  public String removeTitle(String title){
+    if(!this.collection.contains(title)){
+      throw new RuntimeException("This book does not exist in the system!");
+    } else {
+      this.collection.remove(title, true);
+      return title;
+    }
+  }
+
+  public void checkOut(String title){
+    if(!this.collection.get(title)){
+      throw new RuntimeException("This book is not in the system!");
+  } else {
+    this.collection.replace(title, false);
+  }
+}
+  public void returnBook(String title){
+    if(this.collection.get(title)){
+      throw new RuntimeException("This book has already been returned!");
+  } else {
+    this.collection.replace(title, true);
+    }
+  }
+
+  public boolean containsTitle(String title){
+     return this.collection.contains(title);
+  }
+  public boolean isAvailable(String title){
+    return this.collection.get(title);
+
+  }
+  public void printCollection(){
+  }
+
     public static void main(String[] args) {
       new Library();
     }
